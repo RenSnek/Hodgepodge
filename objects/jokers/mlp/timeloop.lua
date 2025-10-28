ef23d90378e0586b60aeaa464293df422900a666
PokeRen <tposefren@gmail.com>
2025-10-28 10:39:16 +0000

    Fixed typo

--- objects/jokers/mlp/timeloop.lua
+++ objects/jokers/mlp/timeloop.lua
@@ -87,7 +87,7 @@ SMODS.Joker {
                     break
                 end
             end
-            if my_pos and G.jokers.cards[my_pos + 1] and not SMODS.is_eternal(G.jokers.cards[my_pos + 1], card) and not G.jokers.cards[my_pos + 1].getting_slicd then
+            if my_pos and G.jokers.cards[my_pos + 1] and not SMODS.is_eternal(G.jokers.cards[my_pos + 1], card) and not G.jokers.cards[my_pos + 1].getting_sliced then
                 local sliced_card = G.jokers.cards[my_pos + 1]
                 sliced_card.getting_sliced = true
                 G.GAME.joker_buffer = G.GAME.joker_buffer + 1
