db80e15a599c4b6c9c897f36e6120c4fcefd12ae
PokeRen <tposefren@gmail.com>
2025-10-31 15:27:41 +0000

    Jumbo deck resprite

--- objects/decks/jimbo.lua
+++ objects/decks/jimbo.lua
@@ -2,7 +2,7 @@ SMODS.Back {
     name = "Jimbo Deck",
     key = "jimbo",
     atlas = "decks_atlas",
-    pos = {x=5,y=0},
+    pos = {x=1,y=0},
     config = {},
     calculate = function(self,back,context)
         if context.initial_scoring_step then
